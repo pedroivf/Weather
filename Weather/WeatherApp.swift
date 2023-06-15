2af07d55dc6158c05a49acb9fae646425ce14602
Pedro Velazquez <pedro.velazquez.fernandez@gmail.com>
2023-06-15 14:21:05 -0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Pedro Velazquez Fernandez on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
